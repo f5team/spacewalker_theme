bdadacc82394f183adea673a9ba97a36b3967315
Jean <jeannguyen.ca@gmail.com>
2018-07-27 23:25:10 -0700

    Change nav

--- js/navscroll.js
+++ js/navscroll.js
@@ -1,7 +1,6 @@
-// sticky Navigation$(document).scroll(function () {
-// jQuery(document).ready(function ($) {
-//   $(document).scroll(function () {
-//     var $nav = $(".site-header");
-//     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-//   });
-// });
+jQuery(document).ready(function ($) {
+  $(document).scroll(function () {
+    var $nav = $(".site-header");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
