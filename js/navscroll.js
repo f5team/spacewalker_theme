64ac606a7bfdea41764b3bf0abe3d9ff7db80b2a
Jean <jeannguyen.ca@gmail.com>
2018-07-29 18:10:16 -0700

    FInish nav

--- js/navscroll.js
+++ js/navscroll.js
@@ -3,4 +3,9 @@ jQuery(document).ready(function ($) {
     var $nav = $(".site-header");
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
+
+	$(".menu-toggle").on("click", function(){
+		$(this).css('background', 'red');
+		$(this).toggleClass("menu-toggle-cross");
+	});
 });
